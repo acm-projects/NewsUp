056e82ecd3b50b4df07395bb593291ea215183f9
Aziz Fraij <aff200000@utdallas.edu>
2022-10-04 16:50:47 -0500

    Sentiment Analysis API Integrated

--- expressbackend/app.js
+++ expressbackend/app.js
@@ -39,3 +39,52 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
+"use strict";
+
+const { TextAnalyticsClient, AzureKeyCredential } = require("@azure/ai-text-analytics");
+const key = 'a118e3ac40d04681a89f1393af5729af';
+const endpoint = 'https://sentimentapi.cognitiveservices.azure.com/';
+// Authenitcation info
+const textAnalyticsClient = new TextAnalyticsClient(endpoint,  new AzureKeyCredential(key));
+
+// Example input
+async function sentimentAnalysisWithOpinionMining(client){
+
+  const sentimentInput = [
+    {
+      text: "https://www.cnn.com/2022/10/04/us/hurricane-ian-florida-recovery-tuesday/index.html",//The food and service were unacceptable. The concierge was nice, however.
+      id: "0",
+      language: "en"
+    }
+  ];
+  const results = await client.analyzeSentiment(sentimentInput, { includeOpinionMining: true });
+
+  for (let i = 0; i < results.length; i++) {
+    const result = results[i];
+    console.log(`- Document ${result.id}`);
+    if (!result.error) {
+      console.log(`\tDocument text: ${sentimentInput[i].text}`);
+      console.log(`\tOverall Sentiment: ${result.sentiment}`);
+      console.log("\tSentiment confidence scores:", result.confidenceScores);
+      console.log("\tSentences");
+      for (const { sentiment, confidenceScores, opinions } of result.sentences) {
+        console.log(`\t- Sentence sentiment: ${sentiment}`);
+        console.log("\t  Confidence scores:", confidenceScores);
+        console.log("\t  Mined opinions");
+        for (const { target, assessments } of opinions) {
+          console.log(`\t\t- Target text: ${target.text}`);
+          console.log(`\t\t  Target sentiment: ${target.sentiment}`);
+          console.log("\t\t  Target confidence scores:", target.confidenceScores);
+          console.log("\t\t  Target assessments");
+          for (const { text, sentiment } of assessments) {
+            console.log(`\t\t\t- Text: ${text}`);
+            console.log(`\t\t\t  Sentiment: ${sentiment}`);
+          }
+        }
+      }
+    } else {
+      console.error(`\tError: ${result.error}`);
+    }
+  }
+}
+sentimentAnalysisWithOpinionMining(textAnalyticsClient);
