58d2995589c9d5436780742f797cd634c90b1ed0
Garrett Hilburn <Garrett.nhil@gmail.com>
2022-10-20 16:52:11 -0500

    sentiment analysis on article titles

--- expressbackend/app.js
+++ expressbackend/app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var fetch = require('node-fetch');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -39,6 +40,10 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
+
+
+// sentiment analysis
+
 "use strict";
 
 const { TextAnalyticsClient, AzureKeyCredential } = require("@azure/ai-text-analytics");
@@ -48,11 +53,11 @@ const endpoint = 'https://sentimentapi.cognitiveservices.azure.com/';
 const textAnalyticsClient = new TextAnalyticsClient(endpoint,  new AzureKeyCredential(key));
 
 // Example input
-async function sentimentAnalysisWithOpinionMining(client){
+async function sentimentAnalysisWithOpinionMining(articleText, client){
 
   const sentimentInput = [
     {
-      text: "https://www.cnn.com/2022/10/04/us/hurricane-ian-florida-recovery-tuesday/index.html",//The food and service were unacceptable. The concierge was nice, however.
+      text: articleText,
       id: "0",
       language: "en"
     }
@@ -87,4 +92,58 @@ async function sentimentAnalysisWithOpinionMining(client){
     }
   }
 }
-sentimentAnalysisWithOpinionMining(textAnalyticsClient);
+
+
+
+//newsapi
+
+const axios = require('axios');
+
+const { JSDOM } = require('jsdom');
+const { Readability } = require('@mozilla/readability');
+
+let url = 'https://newsapi.org/v2/everything?' +
+'q=Dogs&' +
+'sortBy=publishedAt&' +
+'apiKey=c5e6012ef64749fe8ab917786db99c93';
+
+axios.get(url).then(function(r1) { 
+
+  let firstResult = r1.data.articles[0];
+
+  axios.get(firstResult.url).then(function(r2) {
+
+    let dom = new JSDOM(r2.data, {
+      url: firstResult.url
+    });
+
+    let article = new Readability(dom.window.document).parse();
+
+    //sentiment analysis on article title
+    sentimentAnalysisWithOpinionMining(article.title, textAnalyticsClient);
+  })
+})
+
+
+
+//get all news articles
+/*
+
+async function getNews() {
+    var url = 'https://newsapi.org/v2/everything?' +
+          'q=Apple&' +
+          'from=2022-10-18&' +
+          'sortBy=popularity&' +
+          'apiKey=c5e6012ef64749fe8ab917786db99c93';
+
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data);
+}
+
+getNews()
+
+*/
+
+
+
